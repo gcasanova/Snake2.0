f0af1136f59d128593aa7f8cfcf4824407408d5b
Guillermo Casanova <guillerizo@hotmail.com>
2013-10-30 15:18:18 +0100

    second commit

--- Snake2.0/src/main/java/com/mochilo/snake20/framework/Audio.java
+++
@@ -1,9 +0,0 @@
-package com.mochilo.snake20.framework;
-
-/**
- * Created by Mochilo on 26/10/13.
- */
-public interface Audio {
-    public Music newMusic(String filename);
-    public Sound newSound(String filename);
-}
--- Snake2.0/src/main/java/com/mochilo/snake20/framework/Sound.java
+++
@@ -1,9 +0,0 @@
-package com.mochilo.snake20.framework;
-
-/**
- * Created by Mochilo on 26/10/13.
- */
-public interface Sound {
-    public void play(float volume);
-    public void dispose();
-}
